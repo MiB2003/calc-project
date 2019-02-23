187c813c46aa26e5d002354d564280adaea60adc
bvv <bvvtest@mail.ru>
2019-02-23 22:10:07 +0300

    4 commit
    Signed-off-by: bvv <bvvtest@mail.ru>

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,15 +1,39 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
  * create by Bykov V.V.
  */
 
-public class Base extends Calc{
+public class Base extends Calc {
 
-    public static void main(String[] args)  {
-        calc();
+    public static void main(String[] args) {
+        int result = startMenu();
+       if (result==1) calc();
+  }
 
+    public static int startMenu() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Добрый день! \nСделайте выбор:\n" +
+                "1)Запустить Калькулятор.\n" +
+                "2)Поиск максимального элемента в масиве.\n" +
+                "3)Выход");
+        while (true) {
+            try {
+                int result = Integer.valueOf(reader.readLine());
+                if (result == 1 || result == 2 || result == 3)
+                    return result;
+                else throw new IOException();
+            } catch (IOException | NumberFormatException ex) {
+                System.out.println("Ошибка выбора операции.Сделайте выбор и введите номер операции!");
+                System.out.println("1)Запустить Калькулятор.\n" +
+                        "2)Поиск максимального элемента в масиве.\n" +
+                        "3)Выход");
+            }
+        }
     }
 
 
