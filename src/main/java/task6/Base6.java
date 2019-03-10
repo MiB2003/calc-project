b5ca8e324654a673a339998ee7b5268f243f69f2
bvv <bvvtest@mail.ru>
2019-03-10 21:35:02 +0300

    задание 6 правка
    Signed-off-by: bvv <bvvtest@mail.ru>

--- src/main/java/task6/Base6.java
+++ src/main/java/task6/Base6.java
@@ -11,49 +11,39 @@ import java.util.*;
 public class Base6 {
     public static void main(String[] args) {
         File file = new File("C:\\Users\\User\\IdeaProjects\\calc\\src\\main\\resources\\testText.txt");
-        List<String> strings = new ArrayList<String>();
-        Scanner sc = null;
-        int count = 0;
-        int maxNumberWord = 0;
-        String maxWord = null;
-        int lineNumber = 0;
-
+        Scanner scanner = null;
+        int worldCount = 0;
+        Integer maxNumberWord = 0;
+        String word = null;
+        Map<String, Integer> statistics = new TreeMap<>();
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                lineNumber = lineNumber + 1;
-                String line = sc.nextLine().toLowerCase().trim();  //приводим все слова к нижнему регистру и убераем пробелы в начале и конце строки
-                System.out.println("Из файла прочитана строка " + lineNumber + ":");
-                System.out.println("'" + line + "'"); //печать строки в стандартный вывод.
-                System.out.println("Вид строки после сортировки:");
-                List<String> words = new LinkedList<String>(Arrays.asList(line.split("\\s+")));
-                Collections.sort(words); // сортировка списка
-                System.out.println("'" + words + "'"); //печать отсортированного списка слов.
-                System.out.println("В ней встречаются слова седующее число раз:");
-                HashMap<String, Integer> wordToCount = new HashMap<>();
-                for (String word : words) {
-                    count++;
-                    if (!wordToCount.containsKey(word)) {
-                        wordToCount.put(word, 0);
-                    }
-                    wordToCount.put(word, wordToCount.get(word) + 1);
+            scanner = new Scanner(file);
+            System.out.println("Из файла прочитаны следующие слова отсортированные в алфавитном порядке:");
+            while (scanner.hasNext()) {
+                worldCount = worldCount + 1;
+                word = scanner.useDelimiter("\\s+").next();
+                Integer count = statistics.get(word);
+                if (count == null) count = 0;
+                statistics.put(word, ++count);
+                if (count > maxNumberWord) {
+                    maxNumberWord = count;
                 }
-                for (String word : wordToCount.keySet()) {
-                    if (wordToCount.get(word) > maxNumberWord) {
-                        maxWord = word;
-                        maxNumberWord = wordToCount.get(word);
-                    }
-                    System.out.println(word + " -" + wordToCount.get(word) + " раз(а)");
+            }
+            System.out.println(statistics);
+            System.out.println("Всего в списке слов: " + worldCount);
+            System.out.print("Чаще всего, " + maxNumberWord + " раз(а) встречается слово(ва): ");
+            Set<Map.Entry<String, Integer>> entrySet = statistics.entrySet();
+            for (Map.Entry<String, Integer> pair : entrySet) {
+                if (maxNumberWord.equals(pair.getValue())) {
+                    System.out.print("[" + pair.getKey() + "] ");// нашли наше значение и возвращаем  ключ
                 }
-                strings.add(line);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            sc.close();
+            scanner.close();
         }
-        System.out.println("Слово \"" + maxWord + "\" встречается чаще, всего: " + maxNumberWord + " раз(а)");
-        System.out.println("Всего слов: " + count);
+
     }
 }
 
